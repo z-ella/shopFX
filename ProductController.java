e5a6cc08ab163efdfe552f05eea90dab016b7657
z-ella <162042862+z-ella@users.noreply.github.com>
2025-12-15 10:00:23 +0100

    Implement ProductController with table setup

---
+++ ProductController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import dao.ProductDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Product;
+
+public class ProductController {
+
+    @FXML private TableView<Product> table;
+    @FXML private TableColumn<Product, String> colName;
+    @FXML private TableColumn<Product, String> colCategory;
+    @FXML private TableColumn<Product, Double> colPrice;
+    @FXML private TableColumn<Product, Integer> colStock;
+
+    @FXML
+    public void initialize() {
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        colStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+
+        table.getItems().addAll(new ProductDAO().findAll());
+    }
+}
